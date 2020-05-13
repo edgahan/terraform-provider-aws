db615c183967467ac118ddaf39825166acd45f51
root <edgahan@gmail.com>
2020-05-13 22:12:58 +0000

    test automated commit

--- main.tf
+++ main.tf
@@ -15,13 +15,15 @@ data "aws_iam_policy_document" "policy" {
   statement {
     sid    = ""
     effect = "Allow"
-
     principals {
-      identifiers = ["lambda.amazonaws.com"]
-      type        = "Service"
+      identifiers = [
+        "lambda.amazonaws.com",
+      ]
+      type = "Service"
     }
-
-    actions = ["sts:AssumeRole"]
+    actions = [
+      "sts:AssumeRole",
+    ]
   }
 }
 
@@ -31,15 +33,12 @@ resource "aws_iam_role" "iam_for_lambda" {
 }
 
 resource "aws_lambda_function" "lambda" {
-  function_name = "hello_lambda"
-
+  function_name    = "hello_lambda"
   filename         = "${data.archive_file.zip.output_path}"
   source_code_hash = "${data.archive_file.zip.output_base64sha256}"
-
-  role    = "${aws_iam_role.iam_for_lambda.arn}"
-  handler = "hello_lambda.lambda_handler"
-  runtime = "python3.6"
-
+  role             = "${aws_iam_role.iam_for_lambda.arn}"
+  handler          = "hello_lambda.lambda_handler"
+  runtime          = "python3.6"
   environment {
     variables = {
       greeting = "Hello"
